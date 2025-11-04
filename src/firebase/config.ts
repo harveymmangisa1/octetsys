22e5c2729c73345b452e6d966414c53df7918bd0
harvey mmangisa <harveymmangisa@gmail.com>
2025-11-04 08:15:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "octet-insights",
+  "appId": "1:334097226624:web:666c4cb633e24bd93bbf65",
+  "apiKey": "AIzaSyBNgUSiBBvOzrGARJyrKBR2mDOuI3TqJVg",
+  "authDomain": "octet-insights.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "334097226624"
+};
